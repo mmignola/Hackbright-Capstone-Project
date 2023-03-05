1f78e34cb812550168a957472e336b24146083e0
Madeleine Mignola <madeleine.mignola@gmail.com>
2023-03-05 12:54:28 -0800

    moved .py files

---
+++ crafttracker/crud.py
@@ -0,0 +1,96 @@
+"""CRUD operations."""
+
+from crafttracker.model import db, User, Project, Update, connect_to_db
+
+
+def create_user(fname, lname, email, password):
+    """Create and return a new user."""
+
+    user = User(
+        fname = fname, 
+        lname = lname, 
+        email = email, 
+        password = password
+        )
+
+    return user
+
+
+def create_project(user, pattern_link, proj_name, craft_type, proj_type, difficulty, free_pattern, proj_status):
+    """Create and return a new project."""
+
+    project = Project(
+        user = user, 
+        pattern_link = pattern_link,
+        proj_name = proj_name,
+        craft_type = craft_type, 
+        proj_type = proj_type,
+        difficulty = difficulty,
+        free_pattern = free_pattern,
+        proj_status = proj_status
+        )
+
+    return project
+
+
+def create_update(project, update_name, percent_done, notes):
+    """Create and return a new update."""
+
+    update = Update(
+        project = project, 
+        update_name = update_name,
+        percent_done = percent_done,
+        # update_pic_path = update_pic_path,
+        notes = notes
+        )
+
+    return update
+
+
+def get_user_by_email(email):
+    """Return a user, given their email."""
+
+    return User.query.filter(User.email == email).first()
+
+
+def get_users_projects(email):
+    """Return a list of a given user's projects."""
+
+    user = get_user_by_email(email)
+
+    return Project.query.filter(Project.user == user).all()
+
+
+def get_proj_by_id(proj_id):
+    """Returns a project given its id."""
+
+    return Project.query.get(proj_id)
+    
+
+def get_update_by_id(update_id):
+    """Returns an update given its id."""
+
+    return Update.query.get(update_id)
+
+
+def get_updates_by_proj(proj_id):
+    """Returns updates for a given project."""
+
+    project = get_proj_by_id(proj_id)
+
+    return Update.query.filter(Update.project == project).all()
+
+
+def filter_projects(email, craft_type, proj_type, difficulty, proj_status):
+    """Returns a list of projects based on selected filters."""
+
+    user = get_user_by_email(email)
+
+    return Project.query.filter(Project.user == user, Project.craft_type.like(craft_type), Project.proj_type.like(proj_type), Project.difficulty.like(difficulty), Project.proj_status.like(proj_status)).all()
+
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
+
---
+++ crafttracker/model.py
@@ -0,0 +1,86 @@
+"""Models for project organization app."""
+
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement = True, primary_key = True)
+    fname = db.Column(db.String)
+    lname = db.Column(db.String)
+    email = db.Column(db.String, unique = True)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        """Show info about user."""
+
+        return f"<User user_id={self.user_id} fname={self.fname} lname={self.lname} email={self.email} password={self.password}>"
+
+    
+class Project(db.Model):
+    """A project."""
+
+    __tablename__ = "projects"
+
+    proj_id = db.Column(db.Integer, autoincrement = True, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    proj_name = db.Column(db.String, nullable = False)
+    pattern_link = db.Column(db.String, nullable = False)
+    craft_type = db.Column(db.String, nullable = False)
+    proj_type = db.Column(db.String, nullable = False)
+    difficulty = db.Column(db.String, nullable = False)
+    free_pattern = db.Column(db.Boolean, nullable = False)
+    proj_status = db.Column(db.String, nullable = False)
+
+    user = db.relationship("User", backref="projects")
+
+    def __repr__(self):
+        """Show info about project."""
+
+        return f"<Project proj_id={self.proj_id} user_id={self.user_id} proj_name={self.proj_name} pattern_link={self.pattern_link} craft_type={self.craft_type} proj_type={self.proj_type}>"
+
+
+class Update(db.Model):
+    """A project update."""
+
+    __tablename__ = "updates"
+
+    update_id = db.Column(db.Integer, autoincrement = True, primary_key = True)
+    proj_id = db.Column(db.Integer, db.ForeignKey("projects.proj_id"))
+    update_name = db.Column(db.String)
+    percent_done = db.Column(db.Integer)
+    # update_pic_path = db.Column(db.String)
+    notes = db.Column(db.Text)
+
+    project = db.relationship("Project", backref="updates")
+
+    def __repr__(self):
+        """Show info about project updates."""
+
+        return f"<Update update_id={self.update_id} proj_id={self.proj_id} update_name={self.update_name} percent_done={self.percent_done} notes={self.notes}>"
+
+
+def connect_to_db(flask_app, db_uri="postgresql:///projects", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
